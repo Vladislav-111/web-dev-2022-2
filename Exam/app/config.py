1b441c50c1edd0bc00c6ee3c6853aeb05f1d8c89
Vladislav-111 <72132247+Vladislav-111@users.noreply.github.com>
2022-06-25 16:47:42 +0300

    Delete config.py

--- Exam/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SECRET_KEY='299c02dd1e051ca50ebfa7b0e64e4543ec4c611be7dd430caaecabebaf35e702'
-ADMIN_ROLE_ID = 3
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://std_1685_exam:11111112@std-mysql.ist.mospolytech.ru/std_1685_exam"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ECHO = True
-
-UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media', 'images')
